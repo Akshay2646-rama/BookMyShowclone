d5bb758fff5f0e9433bae52f389f8211eca435f2
Akshay <gvrakshay6@gmail.com>
2025-10-08 11:25:51 +0530

    Added liveevents

---
+++ BMSapp 2/BMSapp/LiveEvents.swift
@@ -0,0 +1,18 @@
+//
+//  LiveEvents.swift
+//  BMSapp
+//
+//  Created by Nxtwave on 08/10/25.
+//
+
+import SwiftUI
+
+struct LiveEvents: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LiveEvents()
+}
